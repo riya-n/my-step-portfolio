a44ff6e8e439630384aaac630320892eaaec790e
Riya Narayan <riya.narayan@gmail.com>
2020-06-17 08:00:26 +0000

    remove userCuisineVote class to use generic JsonObject

--- portfolio/src/main/java/com/google/sps/servlets/CuisineDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CuisineDataServlet.java
@@ -1,6 +1,8 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.util.Map;
 import java.util.List;
@@ -17,7 +19,10 @@ import java.lang.Enum;
 
 @WebServlet("/cuisine-data")
 public final class CuisineDataServlet extends HttpServlet {
+  
   private static final Logger log = Logger.getLogger(CuisineDataServlet.class.getName());
+  public static final String CUISINEID_PARAMETER = "cuisineId";
+  public static final String USERID_PARAMETER = "userId";
 
   /** Class that creates an object to hold the cuisineId, cuisineName,
     and the number of votes for that cuisine. */
@@ -52,33 +57,28 @@ public final class CuisineDataServlet extends HttpServlet {
     response.getWriter().println(json);
   }
 
-  /** Class used to parse the json object in post request body. */
-  public class UserCuisineVote {
-    private String userId;
-    private String cuisineId;
-
-    public String getUserId() {
-      return this.userId;
-    }
-
-    public String getCuisineId() {
-      return this.cuisineId;
-    }
-  }
-
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
-    UserCuisineVote cuisineVote = new Gson().fromJson(request.getReader(), UserCuisineVote.class);
+    JsonObject cuisineVote = new Gson().fromJson(request.getReader(), JsonObject.class);
 
-    if (cuisineVote == null) {
+    if (cuisineVote.isJsonNull()) {
       log.severe("cuisine vote should not be null");
       response.setStatus(400);
       return;
     }
 
-    String userId = cuisineVote.getUserId();
-    String cuisine = cuisineVote.getCuisineId();
+    JsonElement userIdEl = cuisineVote.get(USERID_PARAMETER);
+    JsonElement cuisineEl = cuisineVote.get(CUISINEID_PARAMETER);
+
+    if (userIdEl.isJsonNull() || cuisineEl.isJsonNull()) {
+      log.severe("user id and cuisine id should not be null");
+      response.setStatus(400);
+      return;
+    }
+
+    String userId = userIdEl.getAsString();
+    String cuisine = cuisineEl.getAsString();
 
     try {
       AvailableCuisines cuisineId = AvailableCuisines.getFromId(cuisine);
